b35e4f3b1209314282d5390e890fc53aca3bf301
Andrew Mishin <mishin113@gmail.com>
2022-10-18 19:22:04 +0300

    create app

---
+++ src/app/modules/calls/calls.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CallsRoutingModule } from './calls-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CallsRoutingModule
+  ]
+})
+export class CallsModule { }
